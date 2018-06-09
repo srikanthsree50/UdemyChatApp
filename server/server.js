9445ed72713eefc47911b9c8b75d2811ea9fa48b
srikanth <srikanthsree50@gmail.com>
2018-06-09 20:07:25 +0530

    connection commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
+const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http');
+
 const Publicpath = path.join(__dirname,'../public');
 const port = process.env.PORT || 8080
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket) => {
+    console.log('New User connected...');
+
+  socket.on('disconnect',() => {
+    console.log('User was DisConnected .... ');
+        });
+
+});
+
 app.use(express.static(Publicpath));
 
-app.listen(port,() => {
+server.listen(port,() => {
     console.log(`server running at port ${port}`);
 })
