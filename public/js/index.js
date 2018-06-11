8e9130f654b423f3b79a95c331ba705b15013cff
srikanth <srikanthsree50@gmail.com>
2018-06-11 19:34:04 +0530

    outgoing and incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,19 @@ console.log('DisConnected to Server.... ');
 
 socket.on('newMessage',function(message){
 console.log('New Message from client to server',message);
+var li = jQuery('<li></li>');
+li.text(`${message.from}: ${message.text}`);
+
+jQuery('#messages').append(li)
+});
+
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from:'User',
+        text:jQuery('[name=message]').val()
+    },function () {
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ io.on('connection',(socket) => {
 
     socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
 
-                socket.on('createMessage',(message) => {
+                socket.on('createMessage',(message,callback) => {
                     console.log('create Message :',message);
                     io.emit('newMessage',generateMessage( message.from, message.text));
-
+callback('this is from server..');
                     // socket.broadcast.emit('newMessage',{
                     //     from:message.from,
                     //     text:message.text,
