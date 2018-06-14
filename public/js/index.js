4b26f51442cab1785dfc74858a62fca76af54b1f
srikanth <srikanthsree50@gmail.com>
2018-06-14 18:46:26 +0530

    Mustache Templating commit

--- public/js/index.js
+++ public/js/index.js
@@ -10,22 +10,29 @@ console.log('DisConnected to Server.... ');
 });
 
 socket.on('newMessage',function(message){
-console.log('New Message from client to server',message);
-var li = jQuery('<li></li>');
-li.text(`${message.from} : ${message.createdAt} : ${message.text} `);
 
-jQuery('#messages').append(li)
+var template = jQuery('#message-template').html();
+var html = Mustache.render(template,{
+    text:message.text,
+    from:message.from,
+    createdAt:message.createdAt
+});
+jQuery('#messages').append(html);
+
 });
 
 socket.on('newLocationMessage',function(message){
-    var li = jQuery('<li></li>');
-var a = jQuery('<a target = "_blank" style="text-decoration:none">MyCurrentLocation</a>');
 
 
-li.text(` ${message.from} : ${message.createdAt}: `);
-a.attr('href',message.url);
-li.append(a);
-jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template,{
+        from:message.from,
+        url:message.url,
+        createdAt:message.createdAt
+
+    })
+    jQuery('#messages').append(html);
+
 })
 
 jQuery('#message-form').on('submit',function(e){
